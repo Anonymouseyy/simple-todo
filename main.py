08c50e4250c7f4fa5739da56c7704d35416136d6
Anonymouseyy <xieethan2008@gmail.com>
2023-04-09 21:19:21 -0700

    beginnings

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from jinja2.utils import markupsafe
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
