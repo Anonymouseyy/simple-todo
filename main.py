ef67b6016c574abff69ed1003a280fda24c08a84
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2023-04-17 21:56:10 -0700

    beginnings

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from flask import Flask, flash, redirect, render_template, request, session, jso
 from jinja2.utils import markupsafe
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
+from deta import Base, Drive
 
 # Configure application
 app = Flask(__name__)
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+base = Base("todos")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Home Page"""
+
+    return render_template("index.html")
