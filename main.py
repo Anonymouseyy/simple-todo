865fe57e8823d36793cfd4c0211486410933dd18
Ethan Xie <79242981+Anonymouseyy@users.noreply.github.com>
2023-04-21 21:29:03 -0700

    UI mostly done

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-base = Base("todos")
+# base = Base("todos")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route("/", methods=["GET", "POST"])
